771c9c6abbcd19d8d29f8d7639c9d13d98148574
mariavictoriadiaz <diaz.maria@correounivalle.edu.co>
2023-10-24 16:41:48 -0500

    MV-download genesys data and starting cleaning process

---
+++ scripts/r/clean_data/2_cleaning_genesys_data.R
@@ -0,0 +1,123 @@
+# Clean occurrence data for distribution analysis 
+# Maria Victoria Diaz
+# Alliance of Bioversity and CIAT
+#..........................................
+#..........................................
+# Packages ####
+library("data.table")
+library("maptools")
+library("raster")
+library("rgdal")
+library("dismo")
+library("alphahull")
+library("rgeos")
+library("sf")
+library("here")
+library(geodata)
+library(dplyr)
+
+
+set_here()
+
+
+
+# Function
+
+cleaning_process<-function(data, wrld){
+  
+ 
+  cat("Reading institute passport data...", "\n")
+  
+  data <- read.csv(data)
+  
+  names(data)[which(names(data) %in% c("DECLATITUDE", "DECLONGITUDE"))]<- c("Latitude", "Longitude")
+  
+  NAS_data <- data[!complete.cases(data[,c("Latitude", "Longitude")]),]
+  
+  COMPLETE_data <- data[complete.cases(data[,c("Latitude", "Longitude")]),]
+  
+
+  cat("Identifying the accessions with location issues...", "\n")
+  
+    
+  #############################################################################
+  ##### check if countries are the same or the accessions are over the sea ####
+  #############################################################################
+  
+  
+  resultado <- st_join(st_as_sf(COMPLETE_data, coords = c("Longitude", "Latitude"), crs = st_crs(wrld)), wrld); rm(wrld)
+  
+  COMPLETE_data<- data.frame(COMPLETE_data, countries_gadm = resultado$GID_0); rm(resultado)
+    
+  COMPLETE_data$country_issue<-case_when(
+      
+      COMPLETE_data$ORIGCTY == COMPLETE_data$countries_gadm ~ "OK",
+      COMPLETE_data$ORIGCTY != COMPLETE_data$countries_gadm ~ "NO_MATCH",
+      is.na(COMPLETE_data$countries_gadm)  ~ "SEA",
+      .default =  "SEA"
+      
+    )
+    
+  #################################
+  ##### check zero coordinates ####
+  #################################
+    
+  COMPLETE_data$zero_coords<-FALSE
+  COMPLETE_data$zero_coords[which(COMPLETE_data$Longitude == 0 | COMPLETE_data$Latitude == 0)] <- TRUE
+    
+    
+  ####################################
+  ##### check integer coordinates ####
+  ####################################
+    
+  COMPLETE_data$no_decimals<- FALSE
+  COMPLETE_data$no_decimals[which(is.integer(COMPLETE_data$Longitude) | is.integer(COMPLETE_data$Latitude))] <- TRUE
+    
+    
+  #########################################
+  ##### check data without collSite #######
+  #########################################
+    
+    
+  COMPLETE_data$no_collsite <- FALSE
+  COMPLETE_data$no_collsite[is.na(COMPLETE_data$COLLSITE)] <- TRUE
+  
+    
+  NAS_data$no_collsite <- FALSE
+  NAS_data$no_collsite[is.na(NAS_data$COLLSITE)] <- TRUE
+  
+  
+  
+  cat("Returning data", "\n")
+  
+  return(list(YES_coordinates  = COMPLETE_data, NO_coordinates = NAS_data))
+    
+    
+}
+
+##### test ####
+
+files = list.files(here("data"), pattern = ".csv", full.names = T)
+
+gadm <- st_read(here("data/gadm/gadm36_shp/gadm36.shp"))
+gadm <- wrld
+
+
+check <- cleaning_process(data = files[1], wrld = gadm)
+
+si <- check$YES_coordinates
+no <- check$NO_coordinates
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/r/download_genesys_passport_data/1_download_genesys_data.R
@@ -0,0 +1,76 @@
+# Get Genesys occurrence data for distribution analysis 
+# updated by Maria Victoria Diaz and K. de Sousa 
+
+#..........................................
+#..........................................
+
+# Packages ####
+library(genesysr)
+library(data.table)
+library(readxl)
+library(here)
+
+set_here()
+
+# Read institution codes
+
+inst <- read.csv(here("docs","institutions_cgiar.csv"), header = T, sep = ",")
+
+
+
+# Login the Genesys API
+
+
+setup_sandbox()
+user_login()
+
+
+# Define the function to extract passport data from the institute using the API
+
+extract_inst <- function(inst){
+            
+      cat(i, "\n")
+            
+      passport <- download_mcpd(inst)
+            
+      data <- read_excel(passport, sheet = 1)
+            
+            
+      if(nrow(data)>0){
+              
+          write.csv(data, here("data", paste0(gsub(".xlsx", ".csv",passport))), row.names = F)
+              
+        }
+            
+      file.remove(passport)
+            
+      return(data)
+            
+    }
+  
+  
+
+
+
+
+extract_all <- function(inst){
+  
+    
+  # Applying the function extract_inst to every institute in the list 
+  
+  datas <- lapply(inst$INSTCODE, extract)
+  
+  datas <- do.call(rbind, datas)
+  
+  
+  # Saving raw data 
+  
+  write.csv(datas, here('data', 'genesys_downloaded_institutions_data.csv'), row.names = F)
+
+}
+
+  
+
+
+
+  
