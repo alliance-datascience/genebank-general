f19a13c019c61431c08e6d645d803b46dad9e5f2
mariavictoriadiaz <diaz.maria@correounivalle.edu.co>
2023-10-27 09:48:54 -0500

    change genesys environment from test to production

--- scripts/r/download_genesys_passport_data/1_download_genesys_data.R
+++ scripts/r/download_genesys_passport_data/1_download_genesys_data.R
@@ -21,56 +21,56 @@ inst <- read.csv(here("docs","institutions_cgiar.csv"), header = T, sep = ",")
 # Login the Genesys API
 
 
-setup_sandbox()
+setup_production()
 user_login()
 
 
 # Define the function to extract passport data from the institute using the API
 
 extract_inst <- function(inst){
-            
-      cat(i, "\n")
-            
-      passport <- download_mcpd(inst)
-            
-      data <- read_excel(passport, sheet = 1)
-            
-            
-      if(nrow(data)>0){
-              
-          write.csv(data, here("data", paste0(gsub(".xlsx", ".csv",passport))), row.names = F)
-              
-        }
-            
-      file.remove(passport)
-            
-      return(data)
-            
-    }
+  
+  cat(inst, "\n")
   
   
+  passport <- download_mcpd(inst)
+  
+  data <- read_excel(passport, sheet = 1)
+  
+  
+  if(nrow(data)>0){
+    
+    write.csv(data, here("data", paste0(gsub(".xlsx", ".csv",passport))), row.names = F)
+    
+  }
+  
+  file.remove(passport)
+  
+  return(data)
+  
+}
+
+
 
 
 
 
 extract_all <- function(inst){
   
-    
+  
   # Applying the function extract_inst to every institute in the list 
   
-  datas <- lapply(inst$INSTCODE, extract)
+  datas <- lapply(inst$INSTCODE, extract_inst)
   
   datas <- do.call(rbind, datas)
   
   
   # Saving raw data 
   
-  write.csv(datas, here('data', 'genesys_downloaded_institutions_data.csv'), row.names = F)
-
+  write.csv(datas, here('data', 'raw','genesys_downloaded_institutions_data.csv'), row.names = F)
+  
 }
 
-  
 
 
 
-  
+extract_all(inst)
