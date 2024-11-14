25bd77035e6930b417329a5ee7e2784558fef3ed
Mendez <A.C.MENDEZ@cgiar.org>
2024-11-14 13:59:19 -0500

    improved function to download input data from s3

--- scripts/r/00_copy_from_s3.R
+++ scripts/r/00_copy_from_s3.R
@@ -1,3 +1,6 @@
+suppressMessages(if(!require(pacman)){install.packages("pacman");library(pacman)}else{library(pacman)})
+pacman::p_load(data.table, sf, here, geodata, dplyr, stringr, stringi,writexl, arrow, readr,
+               terra, vroom, httr, jsonlite, stringr, cld3, geosphere, readxl, paws, paws.storage)
 
 #' Function to copy country shapefiles files from S3 bucket to EC2 R-studio session
 #' @param s3_clt (object):S3 client from paws.strograge call
@@ -308,6 +311,42 @@ copy_s3_bordering_countries_db_files <- function( s3_clt, out_dir_name){
   
 }
 
+
+#' Function to create necessary folders and execute copy_s3 functions
+#' @param s3_clt (object):S3 client from paws.strograge call
+#' @param root (object): fullpath to root folder
+#' @return NULL
+#' @example get_input_files(s3_clt, root = file.path('path to dir'))
+get_input_files <- function(s3_clt,  root){
+  
+  dir_names <- c("country_shps", "misc", "centroids_data", "decision_tree", "quality_score_data", "country_borders", "score_results")
+  
+  dir_path <- lapply(dir_names, function(pt){
+    fl <- file.path(root, pt)
+    dir.create(fl, recursive = T)
+    return(fl)
+  
+    })
+  
+  copy_s3_country_shp(s3_clt,  file.path(root,"country_shps"))
+  copy_s3_misc_files(s3_clt,  file.path(root,"misc"))
+  copy_s3_centorids_db_files(s3_clt,  file.path(root,"centroids_data"))
+  copy_s3_decision_tree_file(s3_clt,  file.path(root,"decision_tree"))
+  copy_s3_genesys_data(s3_clt,  file.path(root,"quality_score_data"))
+  copy_s3_bordering_countries_db_files(s3_clt,  file.path(root,"country_borders"))
+  
+  try({
+    
+    download.file("https://github.com/alliance-datascience/genebank-general/raw/refs/heads/dev/scripts/r/misc/GCA_data_dictionary.xlsx", 
+                  destfile = file.path(root, "quality_score_data", "GCA_data_dictionary.xlsx"))
+    
+  })
+  
+  
+  return(NULL)
+  
+  
+}
 
 
 
